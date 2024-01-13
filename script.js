1e780b259fb866e274ba06f2456332ee50491256
stacey aoko <staceymarrion@gmail.com>
2024-01-13 18:11:01 +0300

    Fix navbar styling

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const menuIcon = document.querySelector('.menu-icon');
 const menuList = document.querySelector('nav ul');
+const navitems = document.querySelectorAll('.nav-item')
 
-menuIcon.addEventListener('click', () => {
-  menuList.classList.toggle('show');
-});
+// menuIcon.addEventListener('click', () => {
+//   menuList.classList.toggle('show');
+// });
+
+navitems.forEach((item) => {
+  item.addEventListener('click', () => {
+    // Remove 'active' class from all items
+    navitems.forEach((otherItem) => {
+      if (otherItem !== item) {
+        otherItem.classList.remove('active');
+      }
+    });
+
+    // Toggle 'active' class for the clicked item
+    item.classList.add('active');
+  })
+})
